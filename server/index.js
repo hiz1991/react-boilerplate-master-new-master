44fab33d9c8df562c12ce657dcd64d2305d897d2
hiz1991 <hiz1991@mail.ru>
2017-08-23 06:26:39 +0200

    Half way

--- server/index.js
+++ server/index.js
@@ -11,9 +11,6 @@ const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngr
 const resolve = require('path').resolve;
 const app = apiExpress;
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
-
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
