916faf1fb4e7e2bfc5fa3348037dfa054ac847f9
Khizir Putcygov <hiz1991@mail.ru>
2017-08-24 13:55:47 +0200

    Fixed non working inputs

--- app/containers/ConfirmCodePage/index.js
+++ app/containers/ConfirmCodePage/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import store from './store';
 import requestVerify from './showResults';
 import MaterialUiForm from './MaterialUiForm';
 export default class ConfirmCodePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -11,12 +13,14 @@ export default class ConfirmCodePage extends React.Component { // eslint-disable
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <div style={{padding: 15}}>
-          <h2 style={{textAlign: "center"}}>Prodigy.A.I.</h2>
-          <MaterialUiForm onSubmit={val => requestVerify(val, this)} parentComponent={this}/>
-        </div>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <div style={{padding: 15}}>
+            <h2 style={{textAlign: "center"}}>Prodigy.A.I.</h2>
+            <MaterialUiForm onSubmit={val => requestVerify(val, this)} parentComponent={this}/>
+          </div>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import store from './store';
 import requestVerify from './showResults';
 import MaterialUiForm from './MaterialUiForm';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <div style={{padding: 15}}>
-          <h2 style={{textAlign: "center"}}>Prodigy.A.I.</h2>
-          <MaterialUiForm onSubmit={requestVerify} style={{textAlign: "center"}}/>
-        </div>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <div style={{ padding: 15 }}>
+            <h2 style={{textAlign:"center"}}>Prodigy.A.I.</h2>
+            <MaterialUiForm onSubmit={requestVerify} style={{textAlign:"center"}}/>
+          </div>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
